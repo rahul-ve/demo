82d710dc3070326a5c3e7cbe14de38c5608e66ba
Rahul <rv.work@outlook.com>
2023-12-14 19:04:43 +1100

    udpate rebase vs merge demo code

--- misc/git_merge_vs_rebase_conflict.sh
+++ misc/git_merge_vs_rebase_conflict.sh
@@ -1,23 +1,45 @@
 #! /usr/bin/env bash
 
+# NOTE - this script will not work on Mac, depends on gnu tools like gnu-sed, coreutils, bash
+# Install these via homebrew
+# Install gnu tools and set the PATH in .bashrc to pick up gnu ones first
+# refer to homebrew output for the PATH to set
 
 mkdir rebaseVSmerge
 cd rebaseVSmerge/
-git init
-echo 'This is on master branch' > somefile.txt
+git init -b main
+
+echo 'This is on main branch' > somefile.txt
+
 git add .
 git commit -am "added some code"
-git switch -c branchA
+
+git switch -c feature
+
 echo 'This is on feature branch' > somefile.txt
 git commit -am "overwrote some code"
+
 git switch -
-sed -i -e 's/This is on master branch/This is on master branch and I dont want this line modified by anyone!/' somefile.txt
+
+sed -i -e 's/This is on main branch/This is on main branch and do not modify this line!/' somefile.txt
 git commit -am "add more code"
+
 git switch -
-git merge master            # this will throw a conflict
+
+git merge main          # this will throw a conflict
+
 echo -e '\n ***NOTE THE CONFLICT***\n' && cat somefile.txt && echo -e '\n'
+
 git merge --abort
-git rebase master              # this will also throw a conflit but compare the conflict with above!
+
+echo "Now try rebase instead, which will also throw the conflict but the context is flipped"
+
+git rebase main              # this will also throw a conflit but compare the conflict with above!
+
 echo -e '\n ***NOTE THE CONFLICT***\n' && cat somefile.txt && echo -e '\n'
-cd ..
+
+git rebase --abort
+
+# Uncomment below to cleanup
+# cd ..
 # rm -rf rebaseVSmerge              # run this to clean up
